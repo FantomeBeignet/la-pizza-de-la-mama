68fd6781aad447e12a5d1932406a2ae8a4957810
Tom Béné <himself@fantomebeig.net>
2023-04-27 19:43:47 +0200

    feat: type hints

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
-def parse_input(file: str):
-    ingredients = set()
-    clients = []
+def parse_input(file: str) -> tuple[set[str], list[tuple[set[str], set[str]]]]:
+    ingredients: set[str] = set()
+    clients: list[tuple[set[str], set[str]]] = []
     with open(file, "r") as input:
         num_clients = int(input.readline().strip())
-        for i in range(num_clients):
-            likes = set()
-            dislikes = set()
+        for _ in range(num_clients):
+            likes: set[str] = set()
+            dislikes: set[str] = set()
             client_likes = input.readline().strip().split(" ")
             if int(client_likes[0]) > 0:
                 for ingredient in client_likes[1:]:
