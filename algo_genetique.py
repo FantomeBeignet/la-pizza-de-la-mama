ea32a738123b85c60c372bcd753ae9c1488c3c9e
PETIT Thomas <thomas.petit@telecomnancy.eu>
2023-04-30 17:39:35 +0200

    [+] propotion selection

--- algo_genetique.py
+++ algo_genetique.py
@@ -1,10 +1,13 @@
 import random as rd
 from utils import parse_input, satisfied_clients
 
+pop_size = 10
+sel_size = 5
 
-def create_pizzas(ingredients: set[str], n: int) -> list[set[str]]:
+
+def create_pizzas(ingredients: set[str]) -> list[set[str]]:
     pizza_list = []
-    for i in range(n):
+    for i in range(pop_size):
         pizza = set()
         for ing in ingredients:
             if rd.random() > 0.5:
@@ -21,6 +24,18 @@ def is_gen_good(pizza_list: list[set[str]], clients: list[tuple[set[str], set[st
     return True
 
 
+def proportion_selection(pizza_list: list[set[str]], clients: list[tuple[set[str], set[str]]]) -> list[set[str]]:
+    sel_list = pizza_list[:sel_size]
+    for pizza in pizza_list[sel_size:]:
+        for i in range(len(sel_list)):
+            if satisfied_clients(pizza, clients) > satisfied_clients(sel_list[i], clients):
+                sel_list[i] = pizza
+                break
+    return sel_list
+
+
 # print(create_pizzas(parse_input("subject/a_exemple.txt")[0], 3))
 # print(is_gen_good([{"peppers", "cheese"}, {"cheese", "mushrooms", "tomatoes", "peppers", "pineappple"}, {
 #       "peppers", "mushrooms"}], parse_input("subject/a_exemple.txt")[1]))
+# propo_selection(create_pizzas(parse_input("subject/a_exemple.txt")
+#                               [0]), parse_input("subject/a_exemple.txt")[1])
