3a11436bf8b8b3ab2084c870fe62044e3dfbd043
PETIT Thomas <thomas.petit@telecomnancy.eu>
2023-04-30 16:56:41 +0200

    [+] is gen good

--- algo_genetique.py
+++ algo_genetique.py
@@ -1,5 +1,5 @@
 import random as rd
-from utils import parse_input
+from utils import parse_input, satisfied_clients
 
 
 def create_pizzas(ingredients: set[str], n: int) -> list[set[str]]:
@@ -13,4 +13,14 @@ def create_pizzas(ingredients: set[str], n: int) -> list[set[str]]:
     return pizza_list
 
 
-print(create_pizzas(parse_input("subject/a_exemple.txt")[0], 3))
+def is_gen_good(pizza_list: list[set[str]], clients: list[tuple[set[str], set[str]]]) -> bool:
+    ref_score = satisfied_clients(pizza_list[0], clients)
+    for pizza in pizza_list:
+        if satisfied_clients(pizza, clients) != ref_score:
+            return False
+    return True
+
+
+# print(create_pizzas(parse_input("subject/a_exemple.txt")[0], 3))
+# print(is_gen_good([{"peppers", "cheese"}, {"cheese", "mushrooms", "tomatoes", "peppers", "pineappple"}, {
+#       "peppers", "mushrooms"}], parse_input("subject/a_exemple.txt")[1]))
