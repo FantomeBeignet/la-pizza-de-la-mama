c773f164fdba599c5d2100c9ee8836b9607442b5
PETIT Thomas <thomas.petit@telecomnancy.eu>
2023-04-30 18:32:28 +0200

    [+] fill pizza selection

--- algo_genetique.py
+++ algo_genetique.py
@@ -34,7 +34,7 @@ def proportion_selection(pizza_list: list[set[str]], clients: list[tuple[set[str
     return sel_list
 
 
-def crossing(pizza_list: list[set[str]], ingredients: set[str]) -> list[set[str]]:
+def crossing(pizza_list: list[set[str]], ingredients: set[str]):
     for i in range(len(pizza_list) // 2):
         pizza = set()
         for ing in ingredients:
@@ -46,9 +46,19 @@ def crossing(pizza_list: list[set[str]], ingredients: set[str]) -> list[set[str]
         pizza_list.append(pizza)
 
 
+def fill_pizza_selection(pizza_list: list[set[str]], ingredients: set[str]):
+    for i in range(pop_size - len(pizza_list)):
+        pizza = set()
+        for ing in ingredients:
+            if rd.random() > 0.5:
+                pizza.add(ing)
+        pizza_list.append(pizza)
+
+
 # pizza_list = create_pizzas(parse_input("subject/a_exemple.txt")[0])
 # pizza_list = proportion_selection(
 #     pizza_list, parse_input("subject/a_exemple.txt")[1])
-# print(pizza_list)
+# print(pizza_list, len(pizza_list))
 # crossing(pizza_list, parse_input("subject/a_exemple.txt")[0])
-# print(pizza_list)
+# fill_pizza_selection(pizza_list, parse_input("subject/a_exemple.txt")[0])
+# print(pizza_list, len(pizza_list))
