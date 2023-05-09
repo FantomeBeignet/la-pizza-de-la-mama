1916f852880a65860507b78ff186da0e1b714713
Thomas Petit <6poules@gmail.com>
2023-05-09 22:15:04 +0200

    [~] oopsie daisy

--- algo_genetique.py
+++ algo_genetique.py
@@ -4,7 +4,7 @@ import sys
 import math as m
 from utils import parse_input, satisfied_clients, save_solution
 
-pop_size = 50
+pop_size = 500
 sel_size = m.floor(0.6*pop_size)
 global repeat
 repeat = 0
@@ -23,27 +23,29 @@ def create_pizzas(ingredients: set[str]) -> list[set[str]]:
     return pizza_list
 
 
-def is_gen_good(pizza_list: list[set[str]], clients: list[tuple[set[str], set[str]]]) -> bool:
+def is_gen_good(score) -> bool:
     global repeat
-    global last_score
-    if get_max_score(pizza_list, clients) == last_score:
-        repeat += 1
-        if repeat > 10:
-            # print(repeat, last_score)
-            return True
-        else:
-            # print(repeat, last_score)
-            return False
-    else:
-        last_score = get_max_score(pizza_list, clients)
-        repeat = 0
-        # print(repeat, last_score)
-        return False
+    repeat += 1
+    return (repeat > 1000 or score > 2000)
+    # global last_score
+    # if get_max_score(pizza_list, clients) == last_score:
+    #     repeat += 1
+    #     if repeat > 10:
+    #         # print(repeat, last_score)
+    #         return True
+    #     else:
+    #         # print(repeat, last_score)
+    #         return False
+    # else:
+    #     last_score = get_max_score(pizza_list, clients)
+    #     repeat = 0
+    #     # print(repeat, last_score)
+    #     return False
 
 
-def proportion_selection(scored_list: list[int,set[str]], clients: list[tuple[set[str], set[str]]]) -> list[set[str]]:
+def proportion_selection(scored_list: list[int, set[str]]) -> list[set[str]]:
     scored_list.sort()
-    return [ e[1] for e in sorted(scored_list)[:sel_size]]
+    return [e[1] for e in sorted(scored_list)[:sel_size]]
 
 
 def get_max_id(pizza_list: list[set[str]], clients: list[tuple[set[str], set[str]]]):
@@ -81,7 +83,6 @@ def algo_start(input_file: str) -> set[str]:
     global start_time
     ingredients, clients = parse_input(input_file)
     pizza_list = create_pizzas(ingredients)
-    print("created", time.time()-start_time)
     return run_gen_algo(pizza_list, ingredients, clients)
 
 
@@ -89,7 +90,7 @@ def mutate(pizza_list: list[set[str]], ingredients: set[str]):
     for pizza in pizza_list:
         if rd.random() < 0.3:
             for ing in ingredients:
-                if rd.random() > 0.5:
+                if rd.random() > 0.8:
                     if ing in pizza:
                         pizza.remove(ing)
                     else:
@@ -107,11 +108,13 @@ def get_max_score(pizza_list: list[set[str]], clients: list[tuple[set[str], set[
 def run_gen_algo(pizza_list: list[set[str]], ingredients: set[str], clients: list[tuple[set[str], set[str]]]) -> set[str]:
     scored_list = []
     for pizza in pizza_list:
-        scored_list.append([satisfied_clients(pizza,clients),pizza])
-    new_pizza_list = [ e[1] for e in sorted(scored_list)[::-1][:sel_size]]
+        scored_list.append([satisfied_clients(pizza, clients), pizza])
+    scored_list.sort(reverse=True)
+    new_pizza_list = [e[1] for e in scored_list[:sel_size]]
     global start_time
-    print("selected", time.time()-start_time)
-    if is_gen_good(new_pizza_list, clients):
+    print("["+timestamping(time.time() -
+          start_time)+"] generation", repeat+1, "| score :", scored_list[0][0])
+    if is_gen_good(scored_list[0][0]):
         return new_pizza_list[get_max_id(new_pizza_list, clients)]
     else:
         best = new_pizza_list.pop(get_max_id(new_pizza_list, clients))
@@ -121,7 +124,6 @@ def run_gen_algo(pizza_list: list[set[str]], ingredients: set[str], clients: lis
         # print_pizzas(new_pizza_list, clients)
         if len(new_pizza_list) < pop_size:
             fill_pizza_selection(new_pizza_list, ingredients)
-        print("modified", time.time()-start_time)
         return run_gen_algo(new_pizza_list, ingredients, clients)
 
 
@@ -129,6 +131,12 @@ def print_pizzas(pizza_list: list[set[str]], clients: list[tuple[set[str], set[s
     for pizza in pizza_list:
         print(pizza, satisfied_clients(pizza, clients))
     print(get_max_id(pizza_list, clients), "\n")
+
+
+def timestamping(time):
+    min = m.floor(time/60)
+    sec = m.floor(time - min*60)
+    return str(min) + "\'"+str(sec)+"\""
 
 
 if __name__ == '__main__':
